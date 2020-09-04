3979fead035ec0e234d54f96bc98d3fa12d0b043
Fco. Rdz <fco.geovani.rdz@gmail.com>
2020-09-04 18:23:17 -0500

    docs agregado

---
+++ docs/precache-manifest.54c8619a219a67dea8fd2d45cf4a2c86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f1b522660c06894b826175212e3d61f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b28769cb4b53654bfe09",
+    "url": "/static/css/main.61f9ba46.chunk.css"
+  },
+  {
+    "revision": "dcf09d6ede3b15119fcb",
+    "url": "/static/js/2.a16041ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a16041ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b28769cb4b53654bfe09",
+    "url": "/static/js/main.82ef3808.chunk.js"
+  },
+  {
+    "revision": "9d3929294d677b1b9ff6",
+    "url": "/static/js/runtime-main.aa9df6a3.js"
+  }
+]);
